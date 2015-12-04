833afb49af4b5d49d1cefeb41aecad0effd97b8a
nicholas todd <nicholas@art-school.la>
2015-12-03 16:49:27 -0800

    bigger video

--- inlinevideo/js/index.js
+++ inlinevideo/js/index.js
@@ -28,8 +28,8 @@ function loop() {
     // render
     if(elapsed >= ((1000/framesPerSecond)/1000)) {
         video.currentTime = video.currentTime + elapsed;
-        $(canvas).width(window.innerWidth);
-        $(canvas).height(window.innerHeight);
+        $(canvas).width(video.videoWidth);
+        $(canvas).height(video.videoHeight);
         ctx.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
         lastTime = time;
     }
